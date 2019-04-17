855ba74f9bdc57cc6c109c1e9117b0e6de562b9d
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-17 17:16:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  array.each ([]) { |i, arr| arr << i ** 2}
+  array.each ([1, 2, 3]) { |i, arr| arr << i ** 2}
  end
    #square_array([1, 2, 3]) { |i, arr| arr << i **2}
  square_array(1, 2, 3)
