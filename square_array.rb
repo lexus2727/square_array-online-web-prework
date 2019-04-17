fbbcc17e410b9a4809705c50c34e0608c04b939d
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-17 17:35:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
   arr = [] #created new empty array
-  array.each {|num| arr << num ** 2 } #iterate through the other array, array
+  array.each {|num| arr << num ** 2 }
+  
+  #iterate through the other array, 
+  #array which is passed an argument, squaring each argument
   arr
   end
   square_array( [1, 2, 3])
